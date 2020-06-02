0aebdbaf85025f48238893e2d12726f82e5a6d8a
maple3142 <kirby741852963@gmail.com>
2020-06-02 13:31:22 +0800

    remove simple unordered recipe

--- src/net/maple3142/craft2d/item/block/PlankOakBlock.java
+++ src/net/maple3142/craft2d/item/block/PlankOakBlock.java
@@ -4,7 +4,7 @@ import javafx.scene.image.Image;
 import net.maple3142.craft2d.block.Block;
 import net.maple3142.craft2d.block.PlankOak;
 import net.maple3142.craft2d.crafting.Recipe;
-import net.maple3142.craft2d.crafting.SimpleUnorderedRecipe;
+import net.maple3142.craft2d.crafting.SimpleRecipe;
 import net.maple3142.craft2d.item.Item;
 import net.maple3142.craft2d.item.PlaceableItem;
 import net.maple3142.craft2d.item.Stackable;
@@ -13,7 +13,7 @@ public class PlankOakBlock implements PlaceableItem, Stackable {
 
     public static final int id = 5;
 
-    public static final Recipe<PlankOakBlock> recipe = new SimpleUnorderedRecipe<>(PlankOakBlock.class, 4, LogOakBlock.id);
+    public static final Recipe<PlankOakBlock> recipe = new SimpleRecipe<>(PlankOakBlock.class, 4, 1, 1, LogOakBlock.id);
 
     private final Block block = new PlankOak();
 
