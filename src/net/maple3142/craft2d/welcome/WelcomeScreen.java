e050402d0f30649325713482ff898aea509a6710
maple3142 <kirby741852963@gmail.com>
2020-06-11 00:07:57 +0800

    add exit warning

--- src/net/maple3142/craft2d/welcome/WelcomeScreen.java
+++ src/net/maple3142/craft2d/welcome/WelcomeScreen.java
@@ -1,15 +1,23 @@
 package net.maple3142.craft2d.welcome;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import net.maple3142.craft2d.game.Game;
 import net.maple3142.craft2d.game.utils.IOHelper;
+
+import java.io.File;
+import java.util.Optional;
 
 public class WelcomeScreen {
 
@@ -32,6 +40,7 @@ public class WelcomeScreen {
         scene = new Scene(root);
         scene.getStylesheets().add("/welcome/main.css");
 
+
         var fc = new FileChooser();
         var ef = new FileChooser.ExtensionFilter("Craft2d World Files", "*.world");
         fc.getExtensionFilters().add(ef);
@@ -39,26 +48,39 @@ public class WelcomeScreen {
             var file = fc.showSaveDialog(stage);
             if (file == null) return;
             var game = new Game();
-            game.setExitCallback(() -> {
-                IOHelper.writeGameToPath(file.toPath(), game);
-                stage.setScene(scene);
-            });
-            stage.setScene(game.getScene());
-            game.start();
+            startGame(stage, game, file);
         });
         load.setOnAction(event -> {
             var file = fc.showOpenDialog(stage);
             if (file == null) return;
             var game = IOHelper.readGameFromPath(file.toPath());
             if (game == null) return;
-            game.setExitCallback(() -> {
-                IOHelper.writeGameToPath(file.toPath(), game);
-                stage.setScene(scene);
-            });
-            stage.setScene(game.getScene());
-            game.start();
+            startGame(stage, game, file);
         });
         exit.setOnAction(event -> stage.close());
+    }
+
+    private void startGame(Stage stage, Game game, File file) {
+        EventHandler<WindowEvent> closeHandler = event -> {
+            var confirm = new Alert(Alert.AlertType.CONFIRMATION);
+            confirm.setTitle("Exit warning");
+            confirm.setHeaderText("Do you really want to exit without saving?");
+            confirm.setContentText("If you don't, cancel this and press ESC key.");
+            confirm.initModality(Modality.APPLICATION_MODAL);
+            confirm.initOwner(stage);
+            Optional<ButtonType> closeResponse = confirm.showAndWait();
+            if (closeResponse.isEmpty() || closeResponse.get().equals(ButtonType.CANCEL)) {
+                event.consume();
+            }
+        };
+        game.setExitCallback(() -> {
+            IOHelper.writeGameToPath(file.toPath(), game);
+            stage.setScene(scene);
+            stage.setOnCloseRequest(null);
+        });
+        stage.setScene(game.getScene());
+        stage.setOnCloseRequest(closeHandler);
+        game.start();
     }
 
     public Scene getScene() {
